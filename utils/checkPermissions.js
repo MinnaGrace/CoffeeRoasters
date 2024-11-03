1a25bb62db529ac8e4070f0035753e8b9016a633
MinnaGrace <148163245+MinnaGrace@users.noreply.github.com>
2024-11-03 12:45:09 -0500

    fixed bug

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -4,7 +4,7 @@ const checkPermissions = (requestUser, resourceUserId)=>{
     if(requestUser.role === 'admin'){
         return
     }
-    if(requestUser.resourceUserId === resourceUserId.toString())return
+    if(requestUser.userId === resourceUserId.toString())return
 
     else{
         throw new selectedError.unauthorized("not authorized to access this route")
